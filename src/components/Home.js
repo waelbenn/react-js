8de460c625d7f19b919422e08bc776899ac00f27
waelbenn <waelbenngx@outlook.fr>
2022-05-19 09:33:54 -0800

    first checkpoint react

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import NavBar from "./NavBar"
+import Form from "./Form"
+
+function Home() {
+  return (
+  <div>
+    <NavBar/>
+    <Form/>
+    </div>
+  )
+}
+
+export default Home
+
